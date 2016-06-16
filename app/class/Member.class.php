fcbe1d22c1ec1bf227f172a9c916c29ed5199525
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-16 18:22:57 +0200

    Adding new db fields

--- app/class/Member.class.php
+++ app/class/Member.class.php
@@ -2,11 +2,17 @@
 
 class Member {
 	protected $id;
-	protected $login;
+	protected $nickname;
 	protected $email;
 	protected $password;
 	protected $register_time;
 	protected $birthdate;
+	protected $firstname;
+	protected $lastname;
+	protected $phone;
+	protected $sexe;
+	protected $bio;
+	protected $mail_confirmed;
 
 	public function __construct( $playerID ) {
 		
--- app/class/MemberManager.php
+++ app/class/MemberManager.php
@@ -11,12 +11,14 @@ class MemberManager {
 
 	public function add(Membre $membre) {
 		$q = $this->_db->prepare('
-			INSERT INTO user_list(login, email, password, birthdate)
-			VALUES(:login, :email, :password, "birthdate)');
-		$q->bindValue(':login', $membre->getLogin(), PDO::PARAM_STR);
+			INSERT INTO user_list(nickname, email, password, birthdate, firstname, lastname)
+			VALUES(:nickname, :email, :password, :birthdate, :firstname, :lastname)');
+		$q->bindValue(':nickname', $membre->getLogin(), PDO::PARAM_STR);
 		$q->bindValue(':email', $membre->getEmail(), PDO::PARAM_STR);
 		$q->bindValue(':password', $membre->getPassword(), PDO::PARAM_STR);
 		$q->bindValue(':birthdate', $membre->getBirthdate());
+		$q->bindValue(':firstname', $membre->getPassword(), PDO::PARAM_STR);
+		$q->bindValue(':lastname', $membre->getPassword(), PDO::PARAM_STR);
 
 		$q->execute();
 
