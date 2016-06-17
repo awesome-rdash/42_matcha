1614b09ae59c0948e06f76b73654e308b1a210a8
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-17 14:25:16 +0200

    Add form check to be sure that all varirable was passer

--- app/actions/register.action.php
+++ app/actions/register.action.php
@@ -1,2 +1,9 @@
 <?php
-if ($_POST[''])
+
+$toCheck = array("nickname", "email", "password", "password2", "birthdate");
+
+foreach($toCheck as $element)
+if (!isset($_POST[$element]) || empty($_POST[$element])) {
+	header("Location: register.php?error=" . $element);
+}
+echo "OK";
