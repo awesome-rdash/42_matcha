8e764a3a1aab926b4b17c70fc03b92f096911684
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-17 14:46:06 +0200

    Adding action parser

--- app/action.app.php
+++ app/action.app.php
@@ -1,4 +1,11 @@
 <?php
-if (isset($_GET['register'])) {
-	include("app/actions/register.action.php");
+$valid_actions = array("register");
+
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+	$action = $_GET['action'];
+	foreach($valid_actions as $va) {
+		if ($action = $va) {
+			require_once("app/actions/" . $action . ".action.php");
+		}
+	}
 }
