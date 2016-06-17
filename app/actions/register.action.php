95a8a6e23adf0e843a188c83b97757b337b6b5c9
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-17 17:09:09 +0200

    Moved registration check to Member class

--- app/actions/register.action.php
+++ app/actions/register.action.php
@@ -1,9 +1,53 @@
 <?php
 
-$toCheck = array("nickname", "email", "password", "password2", "birthdate");
+$return = Member::newMemberFromRegistration($_POST);
 
-foreach($toCheck as $element)
-if (!isset($_POST[$element]) || empty($_POST[$element])) {
-	header("Location: register.php?error=" . $element);
+if (is_object($return)) {
+	$member = $return;
+
+} else {
+	$error = $return;
 }
-echo "OK";
+
+if (isset($error)) {
+	$error["action"] = "register";
+	switch ($error['type']) {
+		case "non-present": {
+			switch ($error["element"]) {
+				case "nickname":
+					$error["msg"] = "Vous devez entrer un nom d'utilisateur.";
+					break;
+
+				case "email":
+					$error["msg"] = "Vous devez entrer un email valide.";
+					break;
+
+				case "password":
+					$error["msg"] = "Vous devez entrer un mot de passe.";
+					break;
+
+				case "password2":
+					$error["msg"] = "Vous devez entrer la confirmation du mot de passe.";
+					break;
+
+				case "birthdate":
+					$error["msg"] = "Vous devez entrer votre date de naissance.";
+				break;
+
+				default:
+					$error["msg"] = "Un champs n'a pas été rempli.";
+					break;
+			}
+			break;
+			}
+			break;
+
+		case "notthesame":
+			$error['msg'] = "Les mots de passe ne correspondent pas.";
+			break;
+
+		default:
+			$error["msg"] = "Erreur inconnue.";
+			break;
+	}
+}
--- app/class/Member.class.php
+++ app/class/Member.class.php
@@ -17,4 +17,24 @@ class Member {
 	public function __construct( $playerID ) {
 		
 	}
+
+	static public function newMemberFromRegistration( $kwargs ) {
+		$toCheck = array("nickname", "email", "password", "password2", "birthdate");
+
+		foreach($toCheck as $element) {
+			if (!isset($kwargs[$element]) || empty($kwargs[$element])) {
+				$error = array("element" => $element,
+					"type" => "non-present");
+				return $error;
+			}
+		}
+
+		$kwargs['password'] = hash("whirlpool", $kwargs['password']);
+		$kwargs['password2'] = hash("whirlpool", $kwargs['password2']);
+		if ($kwargs['password'] !== $kwargs['password2']) {
+			$error = array("element" => "password",
+				"type" => "notthesame");
+			return $error;
+		}
+	}
 }
