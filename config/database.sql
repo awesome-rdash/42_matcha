6aca01dc5813a7cf7498d844865a30b2ddd0843c
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-20 17:29:57 +0200

    User database added

--- config/database.sql
+++ config/database.sql
@@ -7,39 +7,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
-DROP TABLE IF EXISTS `errors`;
-CREATE TABLE `errors` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `module` varchar(20) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `element` varchar(20) NOT NULL,
-  `message` varchar(255) NOT NULL
+  `nickname` varchar(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `birthdate` date NOT NULL,
+  `firstname` int(11) NOT NULL,
+  `lastname` int(11) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `sexe` bit(2) NOT NULL,
+  `bio` text NOT NULL,
+  `mail_confirmed` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-TRUNCATE TABLE `errors`;
-INSERT INTO `errors` (`id`, `module`, `type`, `element`, `message`) VALUES
-(9, 'register', 'missingfield', 'nickname', 'Vous devez entrer un nom d\'utilisateur.'),
-(10, 'register', 'missingfield', 'email', 'Vous devez entrer votre email.'),
-(11, 'register', 'missingfield', 'password', 'Vous devez entrer un mot de passe.'),
-(12, 'register', 'missingfield', 'password2', 'Vous devez entrer la confirmation du mot de passe.'),
-(13, 'member', 'notthesame', 'password', 'Les mots de passe ne correspondent pas.'),
-(14, 'register', 'missingfield', 'birthdate', 'Vous devez entrer votre date de naissance.'),
-(15, 'member', 'alreadyexist', 'nickname', 'Le nom d\'utilisateur que vous avez choisi est déjà utilisé.'),
-(16, 'member', 'alreadyexist', 'email', 'L\'email que vous avez entrée est déjà associée à un autre compte.'),
-(17, 'member', 'toolong', 'nickname', 'La longueur du nom d\'utilisateur ne peut excéder 15 caractères.'),
-(18, 'member', 'specialchar', 'nickname', 'Le nom d\'utilisateur doit être composé uniquement de caractères alphanumériques.'),
-(19, 'member', 'toolong', 'email', 'Veuillez utiliser une adresse email contenant moins de 255 caractères.'),
-(20, 'member', 'notvalid', 'email', 'L\'adresse email que vous avez entrée n\'est pas valide.'),
-(21, 'member', 'tooshort', 'password', 'Le mot de passe doit faire au moins 6 caractères.'),
-(22, 'member', 'toolong', 'password', 'Le mot de passe ne peut excéder 200 caractères.');
+TRUNCATE TABLE `users`;
 
-
-ALTER TABLE `errors`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 
-ALTER TABLE `errors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
