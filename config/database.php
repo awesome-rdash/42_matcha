20a6f09a0aefd74e2bf951445c42b14246228087
Justin Rouzier <jrouzier@student.42.fr>
2016-06-22 23:19:57 +0200

    Bug correction with username and passwd variables

--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
 <?php
 	$DB_DSN = "mysql:dbname=" . $db_infos['db_name'] . ";host=" . $db_infos['db_host'];
-	$DB_USER = $db_infos['db_user'];
-	$DB_PASSWORD = $db_infos['db_password'];
+	$DB_USER = $db_infos['db_username'];
+	$DB_PASSWORD = $db_infos['db_passwd'];
