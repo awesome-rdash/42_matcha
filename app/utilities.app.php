9f9f22a936b126fa1dcc3c6656b981204b157273
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-23 16:06:48 +0200

    Adding isDigits function

--- app/utilities.app.php
+++ app/utilities.app.php
@@ -2,6 +2,19 @@
 
 Class Utilities {
 	static public function sendMail($to, $subject, $message) {
-		mail($to, $subject, $message);
+		$headers   = array();
+		$headers[] = "MIME-Version: 1.0";
+		$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+		$headers[] = "From: Camagru <no-reply@camagru.fr>";
+		$headers[] = "Subject: {$subject}";
+		$headers[] = "X-Mailer: PHP/".phpversion();
+
+		mail($to, $subject, $message, implode("\r\n", $headers));
+	}
+
+	static public function isDigits($string) {
+		return !preg_match("/[^0-9]/", $element);
 	}
 }
+
+Utilities::sendMail("jrouzier@outlook.com", "test", "test");
