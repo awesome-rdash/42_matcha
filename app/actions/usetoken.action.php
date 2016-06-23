01601c90e0d21bac5088307b2fcebcc19293fc85
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-23 22:13:58 +0200

    usetoken action added

--- app/actions/usetoken.action.php
+++ app/actions/usetoken.action.php
@@ -1,2 +1,33 @@
 <?php
+echo "no redirection <br />";
 
+if (!isset($_GET['token']) || empty($_GET['token'])) {
+	$error = genError('token', 'missing', 'token');
+}
+
+if (!isset($error)) {
+	$manager = new TokenManager($db);
+	$token = $manager->getFromToken(htmlspecialchars($_GET['token']));
+	if (!is_object($token)) {
+		$error = genError('token', 'invalid', 'token');
+	}
+}
+
+if (!isset($error)) {
+	if ($token->isUsed()) {
+		$error = genError('token', 'alreadyused', 'token');
+	}
+	if ($token->getTime_created() < (time() - (60 * 60))) {
+		$error = genError('token', 'outdated', 'token');
+	}
+}
+
+if (!isset($error)) {
+	$manager = new MemberManager($db);
+	$member = $manager->getFromId($token->getId());
+}
+
+switch ($token->getUsefor()) {
+	case "mailconfirmation":
+		break;
+}
