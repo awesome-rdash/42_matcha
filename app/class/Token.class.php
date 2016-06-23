5fd693dfb6afb8d3c838b9d9307df325ce4da626
Justin ROUZIER <jrouzier@student.42.fr>
2016-06-23 16:02:31 +0200

    Adding Token class

---
+++ app/class/Token.class.php
@@ -0,0 +1,26 @@
+<?php
+Class Token {
+	private $_id;
+	private $_user_id;
+	private $_token;
+	private $_time_created;
+	private $_usefor;
+	private $_isUsed;
+
+	use commomMembers;
+
+	public function getId() { return $this->_id; }
+	public function getUser_id() { return $this->_user_id; }
+	public function getToken() { return $this->_token; }
+	public function getTime_created() { return $this->_time_created; }
+	public function getUsefor() { return $this->_usefor; }
+	public function getIsUsed() { return $this->_isUsed; }
+
+	public function serUser_id($id) {
+		if (is_numeric($id)) {
+			$this->_user_id = $id;
+			return true;
+		}
+		return genError("token", "invalid", "userid");
+	}
+}
