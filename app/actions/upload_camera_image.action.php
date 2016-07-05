88a71cc86511e1a5027f217bb6dea24154b3bc5f
Justin Rouzier <jrouzier@student.42.fr>
2016-07-05 16:20:49 +0200

    Image creation in db process

--- app/actions/upload_camera_image.action.php
+++ app/actions/upload_camera_image.action.php
@@ -1,1 +1,18 @@
 <?php
+$toCheck = array("image");
+
+foreach($toCheck as $element) {
+	if (!isset($action[$element]) || empty($action[$element])) {
+		$error = genError("upload_camera_image", "missingparam", $element);
+	}
+}
+
+$image = new UserPicture(0);
+$parameters = array(
+	"owner_id" => $currentUser->getId(),
+	"upload_source" => "camera",
+	"filter_used" => "1");
+$image->hydrate($parameters);
+
+$imageManager = new UserPictureManager($db);
+$imageManager->add($image);
