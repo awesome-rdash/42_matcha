d1c105a81aab330f386aaa2ffcfca23d6481347e
Justin Rouzier <jrouzier@student.42.fr>
2016-07-05 02:32:44 +0200

    Action can now work with POST method

--- action.php
+++ action.php
@@ -1,3 +1,3 @@
 <?php
+
 include("app/init.app.php");
-include("index.php");
--- app/action.app.php
+++ app/action.app.php
@@ -4,13 +4,19 @@ $valid_actions = array(
 	"login",
 	"logout",
 	"useToken",
-	"resetpassword");
+	"resetpassword",
+	"upload_camera_image");
 
 if (isset($_GET['action']) && !empty($_GET['action'])) {
-	$action = $_GET['action'];
+	$action = $_GET;
+} else if(isset($_POST['action']) && !empty($_POST['action'])) {
+	$action = $_POST;
+}
+
+if (isset($action)) {
 	foreach($valid_actions as $va) {
-		if ($action == $va) {
-			require_once("app/actions/" . strtolower($action) . ".action.php");
+		if ($action['action'] == $va) {
+			require_once("app/actions/" . strtolower($action['action']) . ".action.php");
 			if (isset($redirection)) {
 				header("Location: " . basename($_SERVER['PHP_SELF']));
 			}
