5a3553b2db7414e286e545054e579866d4b634d6
Justin Rouzier <jrouzier@student.42.fr>
2016-07-29 01:57:25 +0200

    Adding gallery page files

---
+++ app/pages/gallery.page.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once("app/init.app.php");
+
+$pageTitle = "Galerie";
+$pageStylesheets = array ("main.css", "header.css", "gallery.css");
---
+++ gallery.php
@@ -0,0 +1,4 @@
+<?php
+include("app/pages/gallery.page.php");
+
+include("view/gallery.view.php");
