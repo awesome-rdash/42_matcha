24019d11f07993b93848d6fca81932c5ae4a9dd4
Justin Rouzier <jrouzier@student.42.fr>
2016-07-30 02:57:46 +0200

    Retrieving filters list

--- app/pages/camera.page.php
+++ app/pages/camera.page.php
@@ -4,3 +4,6 @@ include_once("app/init.app.php");
 
 $pageTitle = "Camagru";
 $pageStylesheets = array ("main.css", "header.css", "camera.css");
+
+$filterManager = new FilterManager($db);
+$filters = $filterManager->getList();
