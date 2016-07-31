56f6c24aa191f1271814cc7ba46e5ee704cd69c3
Justin Rouzier <jrouzier@student.42.fr>
2016-07-31 22:15:50 +0200

    Bug correction - two titles in the mail

--- app/class/Utilities.class.php
+++ app/class/Utilities.class.php
@@ -6,7 +6,6 @@ Class Utilities {
 		$headers[] = "MIME-Version: 1.0";
 		$headers[] = "Content-type: text/plain; charset=iso-8859-1";
 		$headers[] = "From: Camagru <no-reply@camagru.fr>";
-		$headers[] = "Subject: {$subject}";
 		$headers[] = "X-Mailer: PHP/".phpversion();
 
 		mail($to, $subject, $message, implode("\r\n", $headers));
