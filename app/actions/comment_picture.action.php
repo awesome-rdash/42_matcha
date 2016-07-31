02436ff92cb0bb34a2cc1e5271e343caad6e78c9
Justin Rouzier <jrouzier@student.42.fr>
2016-07-31 03:42:30 +0200

    Mail sended to the right person

--- app/actions/comment_picture.action.php
+++ app/actions/comment_picture.action.php
@@ -9,24 +9,28 @@ foreach($toCheck as $element) {
 
 if (!isset($error)) {
 	if (isUserLogged()) {
-		$parameters = array(
-			"id_user" => $currentUser->getId(),
-			"id_picture" => $action['id_picture'],
-			"content" => $action['content']);
-		$comment = new Comment(0);
-		$state = $comment->hydrate($parameters);
-		if ($state === true) {
-			$commentManager = new CommentManager($db);
-			$commentManager->add($comment);
-			$imageLink = Utilities::getAdress() . "picture.php?pic=" . $action['id_picture'];
-			$mail = 
-			ucfirst($currentUser->getNickname()) . " a poste un nouveau commentaire sur votre photo : \"" . $comment->getContent() . "\"\nCliquez ici pour le voir : $imageLink";
-			Utilities::sendMail($currentUser->getEmail(),
-				"Votre photo a recu un nouveau commentaire",
-				$mail);
-			echo $comment->getContent();
-		} else {
-			$error = $state;
+		$pm = new UserPictureManager($db);
+		$commentedPic = $pm->get($action['id_picture']);
+		if (is_object($commentedPic)) {
+			$parameters = array(
+				"id_user" => $currentUser->getId(),
+				"id_picture" => $commentedPic->getId(),
+				"content" => $action['content']);
+			$comment = new Comment(0);
+			$state = $comment->hydrate($parameters);
+			if ($state === true) {
+				$commentManager = new CommentManager($db);
+				$commentManager->add($comment);
+				$imageLink = Utilities::getAdress() . "picture.php?pic=" . $commentedPic->getId();
+				$mm = new MemberManager($db);
+				$pic_owner = $mm->get("id", $commentedPic->getOwner_id());
+				$mail = 
+					"Votre photo a recu un nouveau commentaire",
+					$mail);
+				echo $comment->getContent();
+			} else {
+				$error = $state;
+			}
 		}
 	} else {
 		$error = genError("comment", "notlogged", "login");
