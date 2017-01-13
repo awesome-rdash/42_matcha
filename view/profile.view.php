e41646cbc774ec5e2ab86fb8749c5e66a33ab77e
Justin ROUZIER <jrouziestudent.42.fr>
2017-01-13 16:37:52 +0100

    Adding entry point view for profile

---
+++ view/profile.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $pageTitle; ?></title>
+		<meta charset="utf-8">
+		<?php
+			foreach($pageStylesheets as $cssPage) {
+				echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"assets/css/" . $cssPage . "\">";
+			}
+		?>
+	</head>
+	<body>
+		<?php
+			include("view/header.view.php");
+			if (isUserLogged()) {
+				include("view/profile/connected.profile.view.php");
+			} else {
+				include("view/profile/disconnected.profile.view.php");
+			}?>
+	</body>
+</html>
