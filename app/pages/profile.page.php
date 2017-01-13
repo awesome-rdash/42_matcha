e6f0f36e6d798e23ad852c07facbfc792cdf0815
Justin ROUZIER <jrouziestudent.42.fr>
2017-01-13 16:38:41 +0100

    Addind currontProfil variable for use in profile view

--- app/pages/profile.page.php
+++ app/pages/profile.page.php
@@ -4,3 +4,16 @@ include_once("app/init.app.php");
 
 $pageTitle = "Profil";
 $pageStylesheets = array ("main.css", "header.css");
+
+if (isUserLogged()) {
+	if (isset($_GET['member']) && !empty($_GET['member'])) {
+		$memberManager = new MemberManager($db);
+		$currentProfil = $memberManager->get("id", int($_GET['member']));
+		if ($currentProfil === false) {
+			header ("location: profile.php");
+		}
+
+	} else {
+		$currentProfil = $currentUser;
+	}
+}
