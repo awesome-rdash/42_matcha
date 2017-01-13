fb857be0adc9b3319ebe8ca4288b060531429fc1
Justin ROUZIER <jrouziestudent.42.fr>
2017-01-13 16:39:16 +0100

    Adding div structure

--- view/profile/connected.profile.view.php
+++ view/profile/connected.profile.view.php
@@ -1,1 +1,15 @@
-<p>Rien</p>
+<div id="page">
+	<div id="profil_header">
+		<p><?php echo $currentProfil->getLastname(); ?></p>
+	</div>
+	<div id="infos">
+	</div>
+	<div id="map">
+	</div>
+	<div id="history">
+	</div>
+	<div id="photos">
+	</div>
+	<div id="likes">
+	</div>
+</div>
