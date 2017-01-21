a58bfec5a7862e7720dc83831cee5594f102eea3
Justin ROUZIER <jrouziestudent.42.fr>
2017-01-21 21:52:11 +0100

    Adding password boxe

--- view/profile/boxes/header.profile.view.php
+++ view/profile/boxes/header.profile.view.php
@@ -2,12 +2,15 @@
 
 $nameInfos = "<span id=\"lastname\">" . $currentProfile->getLastname() . "</span> <span id=\"firstname\">" . $currentProfile->getFirstname() . "</span>";
 $mailInfo = "Email : <span id=\"email\">" . $currentProfile->getEmail() . "</span>";
+$passwordInfo = "Modifier mon mot de passe";
 
 	if ($ownProfile) {
 		$nameEdit =	"<input type=\"text\" name=\"editLastName\" id=\"editLastName\" value=\"" . $currentProfile->getLastname() . "\" placeholder=\"Nom\" />
 		<input type=\"text\" name=\"editFirstName\" id=\"editFirstName\" placeholder=\"Prénom\" value=\"" . $currentProfile->getFirstname() . "\" /> <input type=\"button\" value=\"Modifier\" onclick=\"updateNames()\" />";
 
-		$mailEdit = "<input type=\"text\" name=\"editEmail\" id=\"editEmail\" value=\"" . $currentProfile->getLastname() . "\" placeholder=\"Email\" /> <input type=\"button\" value=\"Modifier\" onclick=\"updateEmail()\" />"
+		$mailEdit = "<input type=\"text\" name=\"editEmail\" id=\"editEmail\" value=\"" . $currentProfile->getLastname() . "\" placeholder=\"Email\" /> <input type=\"button\" value=\"Modifier\" onclick=\"updateEmail()\" />";
+
+		$passwordEdit = "<input type=\"password\" name=\"editPassword\" id=\"editPassword\" placeholder=\"Votre nouveau mot de passe\" /> <input type=\"button\" value=\"Modifier\" onclick=\"updatePassword()\" />";
 	?>
 
 	<p>
@@ -16,6 +19,7 @@ $mailInfo = "Email : <span id=\"email\">" . $currentProfile->getEmail() . "</spa
 		<div id="score">Mon score de popularité : XXX<br /></div>
 
 		<?php showEditableInfo("email", $mailInfo , $mailEdit) ;?> <br />
+		<?php showEditableInfo("password", $passwordInfo , $passwordEdit) ;?> <br />
 		Modifier mon mot de passe
 	</p>
 <?php
