7af95db7f4ffd0283643df47c349c765568fa979
Justin ROUZIER <jrouziestudent.42.fr>
2017-01-21 20:04:33 +0100

    Adding update profil action

--- app/actions/updateProfilInformations.action.php
+++ app/actions/updateProfilInformations.action.php
@@ -1,7 +1,22 @@
 <?php
 
+$error = false;
+
 if (isset($_POST['data']) && !empty($_POST['data'])) {
-	$data = json_decode($_POST['data']);
+	$data = json_decode($_POST['data'], true);
 } else {
 	$error = "no_data";
+}
+
+if ($error === false) {
+	$memberManager = new MemberManager($db);
+	$parameters = array();
+	$return = $currentUser->hydrate($data);
+	if ($return !== true) {
+		$error = $return;
+	}
+}
+
+if ($error === false) {
+	$memberManager->update($currentUser);
 }
