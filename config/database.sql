2016180f82eb54d42cbede1b66c7cf556e56414e
Justin <jrouzier@outlook.com>
2017-06-03 15:47:19 +0200

    db

--- config/database.sql
+++ config/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 18, 2017 at 02:57 PM
+-- Generation Time: Jun 03, 2017 at 03:46 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.1
 
